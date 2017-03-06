e1065db911f1fcf2da49e31edf23160515d32cb8
Victor Kong <victorkong94@gmail.com>
2017-03-06 15:53:22 -0800

    Remove rownames

--- server.R
+++ server.R
@@ -113,17 +113,16 @@ function(input, output, session) {
     finalDrug = sapply(1:nrow(gennm), function(x) gennm[x, visits[x] - 1])
     
     # Save processed data to csv file
-    data = matrix(c(IDcolumn, PDD, PDDfactor, visits, RxChange, RxIncrease,
-                    RxDecrease, totalChange, ratioIncreaseChange, drugs,
-                    mainDrug, initialDrug, finalDrug),
-                  ncol = 19)
-    colnames(data) = c(input$IDcolumn, "PDD/DDD", "PDD/DDD Factor",
-                       "Number of Visits", "PDD/DDD Changes",
-                       "PDD/DDD Increases", "PDD/DDD Decreases",
-                       "Total PDD/DDD Changes", "Increase/Change Ratio",
-                       "Atorva", "Fluva", "Lova", "Pitava", "Prava", "Rosuva",
-                       "Simva", "Primary Drug", "Initial Drug", "Final Drug")
-    rownames(data) = NULL
+    data = cbind(PDD, PDDfactor, visits, RxChange, RxIncrease,
+                 RxDecrease, totalChange, ratioIncreaseChange, drugs,
+                 mainDrug, initialDrug, finalDrug)
+    colnames(data) = c("PDD/DDD", "PDD/DDD Factor", "Number of Visits",
+                       "PDD/DDD Changes", "PDD/DDD Increases",
+                       "PDD/DDD Decreases", "Total PDD/DDD Changes",
+                       "Increase/Change Ratio", "Atorva", "Fluva", "Lova",
+                       "Pitava", "Prava", "Rosuva", "Simva", "Primary Drug",
+                       "Initial Drug", "Final Drug")
+    rownames(data) = IDcolumn
     filename = strsplit(input$datafile$name, "\\.")[[1]]
     filename = paste0(filename[1], " (Processed).", filename[2])
     
